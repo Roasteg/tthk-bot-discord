7fae60528eec91737686e939d7775d3d095d1b8a
Roasteg <krostislav468@gmail.com>
2021-05-11 22:04:37 +0300

    Initial commit

---
+++ commands/consultations.js
@@ -0,0 +1,53 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const axios = require('axios');
+
+let consultations = [];
+
+let times = "";
+let email = "";
+
+
+axios.get('https://api.bredbrains.tech/consultations').then(response => {
+    consultations = response.data.data;
+    console.log(consultations);
+})
+
+
+
+module.exports = {
+    name: 'tthk',
+    description: 'Show consultations',
+    execute(msg, args) {
+        times = "";
+        let result = consultations.filter(x => x.teacher.includes(args));
+        if (!result.length) {
+            let fail = new Discord.MessageEmbed()
+                .setColor('#E74C3C')
+                .setTitle('Error!')
+                .setDescription('No such teacher, maybe, try again?')
+
+            return msg.channel.send(fail)
+        }
+        for (i = 0; i < result[0].times.length; i++) {
+            times += `${result[0].times[i].weekday}, ${result[0].times[i].time}; `
+        }
+
+
+
+        let success = new Discord.MessageEmbed()
+            .setColor('#2ECC71')
+            .setTitle('Teacher found!')
+            .setDescription(`Hey ${msg.author.toString()}, here is the results.`)
+            .addFields(
+                { name: "Name", value: result[0].teacher },
+                { name: "Room", value: result[0].room },
+                { name: "Email", value: result[0].email },
+                { name: "Department", value: result[0].department },
+                { name: "Times", value: times }
+            )
+        msg.channel.send(success);
+    }
+}
+
+
---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = process.env.DISCORD_TOKEN;
+const fs = require('fs');
+const prefix = process.env.PREFIX;
+bot.login(TOKEN);
+
+bot.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  bot.commands.set(command.name, command);
+}
+
+bot.on('message', message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).trim().split(' ');
+  const command = args.shift().toLowerCase();
+  switch (command) {
+    case 'tthk':
+      if(!args.length){
+        return message.channel.send("Looks like you forgot to mention a teacher");
+      }
+      bot.commands.get('tthk').execute(message, args);
+      break;
+  }
+});
+
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
